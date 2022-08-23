92cf34cc96e07e0844bab1389ea1ee489fe01a22
hp <hp@LAPTOP-TE30B2CR>
2022-08-23 12:44:53 +0530

    My Second Commit

---
+++ Patternjava/src/anudip/PatternJava.java
@@ -0,0 +1,32 @@
+package anudip;
+
+public class PatternJava {
+
+	/*
+	 * This program is for printing a number pattern shown below:
+	 * 1
+	 * 121
+	 * 12321
+	 * 1234321
+	 */
+    public static void main(String[] args)
+    {
+        
+        System.out.println("** Printing the pattern... **");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            for (int k = i - 1; k >= 1; k--)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+
+	}
+
---
+++ Patternjava/src/module-info.java
@@ -0,0 +1,2 @@
+module Patternjava {
+}
