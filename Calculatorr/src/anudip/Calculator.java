bb9e472fb068fdb0c2e2f92e453366d2c7c7c528
hp <hp@LAPTOP-TE30B2CR>
2022-08-23 11:58:22 +0530

    My First Commit

---
+++ Calculatorr/src/anudip/Calculator.java
@@ -0,0 +1,61 @@
+package anudip;
+
+	import java.util.*;
+	public class Calculator {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			char operator;
+		    Double number1, number2, result;
+
+		    // create an object of Scanner class
+		    Scanner input = new Scanner(System.in);
+
+		    
+		    // ask users to enter numbers
+		    System.out.println("Enter first number:");
+		    number1 = input.nextDouble();
+
+		    System.out.println("Enter second number:");
+		    number2 = input.nextDouble();
+		    
+		 // ask users to enter operator
+		    System.out.println("Choose an operator: +, -, *, or /");
+		    operator = input.next().charAt(0);
+
+		    
+		    switch (operator) {
+
+		      // performs addition between numbers
+		      case '+':
+		        result = number1 + number2;
+		        System.out.println(number1 + " + " + number2 + " = " + result);
+		        break;
+
+		      // performs subtraction between numbers
+		      case '-':
+		        result = number1 - number2;
+		        System.out.println(number1 + " - " + number2 + " = " + result);
+		        break;
+
+		      // performs multiplication between numbers
+		      case '*':
+		        result = number1 * number2;
+		        System.out.println(number1 + " * " + number2 + " = " + result);
+		        break;
+		        
+		     // performs division between numbers
+		      case '/':
+		        result = number1 / number2;
+		        System.out.println(number1 + " / " + number2 + " = " + result);
+		        break;
+
+		      default:
+		        System.out.println("Invalid operator!");
+		        break;
+		    }
+
+		}
+
+	}
+
